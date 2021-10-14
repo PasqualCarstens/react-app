af261cf6bb59a42996986d3dad92edcfe574f418
PasqualCarstens <pasqualcarstens@gmail.com>
2021-10-14 16:08:01 +1100

    Added search engine and API integration

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <h1>Weather App</h1>
+      <Weather />
     </div>
   );
 }
